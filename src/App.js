9665ff9d9ce7d84380bcc8181f42b83252b74135
David Narvaez <david.ibanezn@gmail.com>
2021-08-10 17:16:39 -0500

    hashrouter

--- src/App.js
+++ src/App.js
@@ -3,11 +3,11 @@ import { Nav } from './components/Nav';
 import { Home } from './views/Home';
 import { Projects } from './views/Projects';
 import { About } from './views/About';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { HashRouter as Router, Switch, Route } from 'react-router-dom';
 
 export const App = () => {
   return (
-    <Router basename={process.env.PUBLIC_URL}>
+    <Router>
       <div className="App">
         <Nav />
         <Switch>
